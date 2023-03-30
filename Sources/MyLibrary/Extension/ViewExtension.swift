bcc57a207931831c1f0ac96172ea5d11b340b218
BlueSkyAnimal <101543434+BlueSkyAnimal@users.noreply.github.com>
2023-03-30 21:44:02 +0900

    Viewのextensionにifモディファイアを追加。

--- Sources/MyLibrary/Extension/ViewExtension.swift
+++ Sources/MyLibrary/Extension/ViewExtension.swift
@@ -42,5 +42,23 @@ public extension View {
             self
         }
     }
+    
+    @ViewBuilder
+    func hidden(_ isHidden: Bool) -> some View {
+        if isHidden {
+            hidden()
+        } else {
+            self
+        }
+    }
+    
+    @ViewBuilder
+    func `if`<Transform: View>(_ condition: Bool, transform: (Self) -> Transform) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
 }
 #endif
