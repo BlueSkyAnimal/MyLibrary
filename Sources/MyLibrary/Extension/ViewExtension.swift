6a41dbaf43710ab82908041917616c143ac4d2e7
BlueSkyAnimal <101543434+BlueSkyAnimal@users.noreply.github.com>
2023-04-06 17:33:16 +0900

    軽微な修正。

--- Sources/MyLibrary/Extension/ViewExtension.swift
+++ Sources/MyLibrary/Extension/ViewExtension.swift
@@ -53,12 +53,8 @@ public extension View {
     }
     
     @ViewBuilder
-    func `if`<Transform: View>(_ condition: Bool, transform: (Self) -> Transform) -> some View {
-        if condition {
-            transform(self)
-        } else {
-            self
-        }
+    func transform<Transform: View>(_ transform: (Self) -> Transform) -> some View {
+        transform(self)
     }
 }
 #endif
